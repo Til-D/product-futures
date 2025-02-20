1707b8d8f2dceabf9b2a9e6aca4c4a9cb6d0e9e4
Tilman <tilman@tilmanification.com>
2025-02-20 07:19:54 +0100

    Update fetch-news.py
    remove api key

--- fetch-news.py
+++ fetch-news.py
@@ -1,6 +1,6 @@
 import requests  # Library for making web requests
 
-API_KEY = "0780ffa381f44946a9b3e406eee00264"  # Replace with your API key
+API_KEY = "your-api-key"  # Replace with your API key
 url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}"
 
 response = requests.get(url)  # Send a request to the API
